fdcfeec425e50665136a582ed3caa6c36c375d1f
Daniel <dannyblaze469@gmail.com>
2021-06-30 16:29:20 -0400

    commit tree 098f86df1d21c79351b9acaef0752a9e5dbfdbc3

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return [arr[0],arr[1]]
+}
+
+const returnLastTwoDrivers = (arr) => {
+    // let rev = arr.reverse()
+    // return [rev[1], rev[0]]
+    return [arr[2], arr[3]]
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(int){
+    return function(){
+        return int * int
+    }
+}
+
+function fareDoubler(cash){
+    // const fare = createFareMultiplier(cash)
+    return cash * 2
+}
+
+function fareTripler(fare){
+    return fare * 3
+}
+
+function selectDifferentDrivers(arr, func){
+    return func(arr)
+}
